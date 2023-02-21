07d0e37db3bcf8c685f29c000c50ca178ee59520
SATHISH-GITU <111628320+SATHISH-GITU@users.noreply.github.com>
2023-02-21 21:28:14 +0530

    Update Func.js

--- Func.js
+++ Func.js
@@ -1,10 +1,33 @@
 function Area() {
+    var a=5;
+    var b=10;
+    var h=6;
+    var B = 10;
+    var H = 6;
+    var A = 5;
+    var pi = 3.14;
+    var r = 10;
+    var c=(a+b)/h;
     return (
+
      <div>
-        
-         <p>AREA OF THE SHAPES</p>
+         <h5><mark>Given Data</mark></h5>
+         <h5 id="shap" > "For Trapezoid" var a=5;
+             var b=10;<br></br>
+             For Parallelogram:
+             var pi = 3.14;
+             var r = 10;
+             </h5>
+         `<h4>AREA OF THE Trapezoid :<br></br> <mark>ANS:{c}</mark> <br></br>
+         Area Of The Parallelogram :<br></br>
+         <mark> ANS:{(B*H)}</mark><br></br>
+         Area Of The Square:<br></br><mark>ANS:{(A*A)}</mark><br></br>
+         Area of The Circle:<br></br><mark>ANS: {(pi*r*r)}</mark>
+         
+          </h4>`
+          
      </div>
     );
   }
   
-  export default Area;
+  export default Area; 
